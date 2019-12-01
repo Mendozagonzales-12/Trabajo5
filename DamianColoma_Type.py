348a31e18d41d91ad130d1ec653e8c9bc5fc2dbf
Mendozagonzales-12 <57878122+Mendozagonzales-12@users.noreply.github.com>
2019-12-01 14:21:47 -0500

    Se arego tipo de datos de Damian Coloma

---
+++ DamianColoma_Type.py
@@ -0,0 +1,489 @@
+#Ejercicio01
+deuda_promedio=124.56
+type_deuda_promedio=type(deuda_promedio)
+deuda_promedio_es_float=isinstance(deuda_promedio,float)
+deuda_promedio_es_str=isinstance(deuda_promedio,str)
+print("Variablle de deuda promedio es :" , type_deuda_promedio)
+print("Variable es float:", deuda_promedio_es_float)
+print("variable es str:", deuda_promedio_es_str)
+
+
+#Ejercicio02
+cuota=76.56
+type_cuota=type(cuota)
+cuota_es_float=isinstance(cuota,float)
+cuota_es_int=isinstance(cuota,int)
+print("variable de cuota es:",type_cuota)
+print("variable es float:",cuota_es_float)
+print("variable es int:", cuota_es_int)
+
+#Ejercicio03
+producto_bruto_interno=1897
+type_producto_bruto_interno=type(producto_bruto_interno)
+producto_bruto_interno_es_int=isinstance(producto_bruto_interno,int)
+producto_bruto_interno_es_float=isinstance(producto_bruto_interno,float)
+print("variable de producto bruto interno es:",type_producto_bruto_interno)
+print("variable es int:",producto_bruto_interno_es_int)
+print("variable es float:",producto_bruto_interno_es_float)
+
+
+#Ejercicio04
+capital_inicial=15689
+type_capital_inicial=type(capital_inicial)
+capital_inicial_es_int=isinstance(capital_inicial,int)
+capital_inicial_es_str=isinstance(capital_inicial,str)
+print("Variable de capital inicial es:",type_capital_inicial)
+print("Variable es int:",capital_inicial_es_int)
+print("Variable es str:"capital_inicial_es_str)
+
+#Ejercicio05
+mano_obra=1234.54
+type_mano_obra=type(mano_obra)
+mano_obra_es_float=isinstance(mano_obra,int)
+mano_obra_es_int=isinstance(mano_obra,int)
+print("Variable de mano de obra es:",type_mano_obra)
+print("Variable es float:",mano_obra_es_float)
+print("Variable es int:",mano_obra_es_int)
+
+#Ejercicio06
+costo_venta=1456
+type_costo_venta=type(costo_venta)
+costo_venta_es_int=isinstance(costo_venta,int)
+costo_venta_es_float=isinstance(costo_venta,float)
+print("Variable de costo de venta es:",type_costo_venta)
+print("Variable es int:",costo_venta_es_int)
+print("Variable es float:",costo_venta_es_float)
+
+#Ejercicio07
+monto=189
+type_monto=type(monto)
+monto_es_int=isinstance(monto,int)
+monto_es_str=isinstance(monto,str)
+print("Variable de monto es:",type_monto)
+print("Variable es int:",monto_es_int)
+print("Variable es str:",monto_es_str)
+
+#Ejercicio08
+comision=132.56
+type_comision=type(comision)
+comision_es_float=isinstance(comision,float)
+comision_es_str=isinstance(comision,str)
+print("Variable de comision es:",type_comision)
+print("Variable es float:",comision_es_float)
+print("variable es str:",comision_es_str)
+
+#Ejercicio09
+saldo_capital=6786
+type_saldo_capital=type(saldo_capital)
+saldo_capital_es_int=isinstance(saldo_capital,int)
+saldo_capital_es_str=isinstance(saldo_capital,str)
+print("Variable de saldo capital es:",type_saldo_capital)
+print("Variable es int:",saldo_capital_es_int)
+print("Variable es str:",saldo_capital_es_str)
+
+#Ejercicio10
+deuda_bcp=16757
+type_deuda_bcp=type(deuda_bcp)
+deuda_bcp_es_int=isinstance(deuda_bcp,int)
+deuda_bcp_es_float=isinstance(deuda_bcp,float)
+print("Variable de deuda bcp es:",type_deuda_bcp)
+print("Variable es int:",deuda_bcp_es_int)
+print("Variable es float:",deuda_bcp_es_float)
+
+#Ejercicio11
+cuotas_año=5
+type_cuotas_año=type(cuotas_año)
+cuotas_año_es_int=isinstance(cuotas_año,int)
+cuotas_año_es_float=isinstance(cuotas_año,float)
+print("Variable de cuotas al año es:",type_cuotas_año)
+print("Variable es int:",cuotas_año_es_int)
+print("Variable es float:",cuotas_año_es_float)
+
+#Ejercicio12
+importe_desembolsado=1234.876
+type_importe_desembolsado=type(importe_desembolsado)
+importe_desembolsado_es_float=isinstance(importe_desembolsado,int)
+importe_desembolsado_es_str=isinstance(importe_desembolsado,str)
+print("Variable de importe desembolsado es:",type_importe_desembolsado)
+print("Variable es float:",importe_desembolsado_es_float)
+print("Variable es str:",importe_desembolsado_es_str)
+
+#Ejercicio13
+penalidad=13549.99
+type_penalidad=type(penalidad)
+comision_es_float=isinstance(penalidad,float)
+comision_es_str=isinstance(penalidad,str)
+print("Variable de penalidad es:",type_penalidad)
+print("Variable es float:",penalidad_es_float)
+print("variable es str:",penalidad_es_str)
+
+#Ejercicio14
+gasto_publico=6438.98
+type_gasto_publlico=type(gasto_publico)
+gasto_publico_es_float=isinstance(gasto_publico,float)
+gasto_publico_es_int=isinstance(gasto_publico,int)
+print("variable de gasto publico es:",type_gasto_publlico)
+print("variable es float:",gasto_publico_es_float)
+print("variable es int:", gasto_publico_es_int)
+
+
+#Ejercicio15
+periodo_pago=8
+type_periodo_pago=type(type_periodo_pago)
+monto_es_int=isinstance(periodo_pago,int)
+monto_es_str=isinstance(periodo_pago,str)
+print("Variable de perido de pago es:",type_periodo_pago)
+print("Variable es int:",periodo_pago_es_int)
+print("Variable es str:"periodo_pago_es_str)
+
+
+#Ejercicio16
+deuda=6543.94
+type_deuda=type(deuda)
+deuda_es_float=isinstance(deuda,float)
+deuda_es_str=isinstance(deuda,str)
+print("Variable de deuda es:",type_deuda)
+print("Variable es float:",deuda_es_float)
+print("variable es str:",deuda_es_str)
+
+#Ejercicio17
+tda=154.98
+type_tda=type(tda)
+tda_es_float=isinstance(tda,float)
+tda_es_int=isinstance(td,int)
+print("variable de tda es:",type_tda)
+print("variable es float:",tda_es_float)
+print("variable es int:", tda_es_int)
+
+
+#Ejercicio18
+capital_prestamo=7000
+type_capital_prestamo=type(capital_prestamo)
+capital_prestamo_es_int=isinstance(capital_prestamo,int)
+capital_prestamo_es_str=isinstance(capital_prestamo,str)
+print("Variable de capital prestamo es:",type_capital_prestamo)
+print("Variable es int:",capital_prestamo_es_int)
+print("Variable es str:",capital_prestamo_es_str)
+
+
+#Ejercicio19
+tasa_mensual=0.99
+type_tasa_mensual=type(tasa_mensual)
+tasa_mensual_es_float=isinstance(tasa_mensual,float)
+tasa_mensual_es_str=isinstance(tasa_mensual,str)
+print("Variable de tasa mensual es :" , type_tasa_mensual)
+print("Variable es float:", tasa_mensual_es_float)
+print("variable es str:", tasa_mensual_es_str)
+
+
+#Ejercicio20
+seguro_desgravamen=687.10
+type_seguro_desgravamen=type(seguro_desgravamen)
+seguro_desgravamen_es_float=isinstance(seguro_desgravamen,float)
+seguro_desgravamen_es_int=isinstance(seguro_desgravamen,int)
+print("variable de seguro desgravamen es:",type_cuota)
+print("variable es float:",seguro_desgravamen_es_float)
+print("variable es int:", seguro_desgravamen_es_int)
+
+#Ejercicio21
+inversion_efectuada=5000
+type_inversion_efectuada=type(inversion_efectuada)
+inversion_efectuada_es_int=isinstance(inversion_efectuada,int)
+inversion_efectuada_es_float=isinstance(inversion_efectuada,float)
+print("variable de inversion efectuada es:",type_inversion_efectuada)
+print("variable es int:",inversion_efectuada_es_int)
+print("variable es float:",inversion_efectuada_es_float)
+
+#Ejercicio22
+tiempo=15
+type_tiempol=type(tiempo)
+tiempo_es_int=isinstance(tiempo,int)
+tiempo_es_str=isinstance(tiempo,str)
+print("Variable de tiempo es:",type_tiempo)
+print("Variable es int:",tiempo_es_int)
+print("Variable es str:",tiempo_es_str)
+
+#Ejercicio23
+exportaciones=1234.54
+type_exportaciones=type(exportaciones)
+exportaciones_es_float=isinstance(exportaciones,int)
+exportaciones_es_int=isinstance(exportaciones,int)
+print("Variable de exportaciones es:",type_exportaciones)
+print("Variable es float:",exportaciones_es_float)
+print("Variable es int:",exportaciones_es_int)
+
+#Ejercicio24
+existencia_final=543
+type_existencia_final=type(existencia_final)
+existencia_final_es_int=isinstance(existencia_final,int)
+existencia_final_es_float=isinstance(existencia_final,float)
+print("Variable de existencia final es:",type_existencia_final)
+print("Variable es int:",existencia_final_es_int)
+print("Variable es float:",existencia_final_es_float)
+
+
+
+#Ejercicio25
+producto_proceso=654.87
+type_producto_proceso=type(producto_proceso)
+producto_proceso_es_float=isinstance(producto_proceso,float)
+producto_proceso_es_str=isinstance(producto_proceso,str)
+print("Variable de producto proceso es :" ,type_producto_proceso)
+print("Variable es float:", producto_proceso_es_float)
+print("variable es str:", producto_proceso_es_str)
+
+
+#Ejercicio26
+dias_base_anual=15
+type_dias_base_anualo=type(dias_base_anual)
+dias_base_anual_es_int=isinstance(dias_base_anual,int)
+dias_base_anual_es_float=isinstance(dias_base_anual,float)
+print("variable de dias base anual es:",type_dias_base_anual)
+print("variable es int:",producto_dias_base_anual_es_int)
+print("variable es float:",producto_dias_base_anual_es_float)
+
+
+
+#Ejercicio27
+materia_directa_final_disponible=1423.87
+type_materia_directa_final_disponible=type(materia_directa_final_disponible)
+materia_directa_final_disponible_es_float=isinstance(materia_directa_final_disponible,float)
+materia_directa_final_disponible_es_str=isinstance(materia_directa_final_disponible,str)
+print("Variable de materia directa final disponible es :" ,type_materia_directa_final_disponible)
+print("Variable es float:", materia_directa_final_disponible_es_float)
+print("variable es str:", materia_directa_final_disponible_es_str)
+
+
+#Ejercicio28
+existencia_inicial=6438.98
+type_existencia_inicial=type(existencia_inicial)
+existencia_inicial_es_float=isinstance(existencia_inicial,float)
+existencia_inicial_es_int=isinstance(existencia_inicial,int)
+print("variable de existencia inicial es:",type_existencia_inicial)
+print("variable es float:",existencia_inicial_es_float)
+print("variable es int:", existencia_inicial_es_int)
+
+
+
+#Ejercicio29
+materia_prima_directa=167.98
+type_materia_prima_directa=type(materia_prima_directa)
+materia_prima_directa_es_float=isinstance(materia_prima_directa,float)
+materia_prima_directa_es_str=isinstance(materia_prima_directa,str)
+print("Variable de materia prima directa es :" ,type_materia_prima_directa)
+print("Variable es float:", materia_prima_directa_es_float)
+print("variable es str:", materia_prima_directa_es_str)
+
+
+
+#Ejercicio30
+cambio_dia_hoy=0.5
+type_cambio_dia_hoy=type(cambio_dia_hoy)
+cambio_dia_hoy_es_float=isinstance(cambio_dia_hoy,float)
+cambio_dia_hoy_es_int=isinstance(cambio_dia_hoy,int)
+print("variable de cambio dia hoy es:",type_cambio_dia_hoy)
+print("variable es float:",cambio_dia_hoy_es_float)
+print("variable es int:", cambio_dia_hoy_es_int)
+
+
+#Ejercicio31
+producto_proceso=345.76
+type_producto_proceso=type(producto_proceso)
+producto_proceso_es_float=isinstance(producto_proceso,float)
+producto_proceso_es_str=isinstance(producto_proceso,str)
+print("Variable de producto proceso es :" ,type_producto_proceso)
+print("Variable es float:", producto_proceso_es_float)
+print("variable es str:", producto_proceso_es_str)
+
+
+#Ejercicio32
+tipo_cambio_dia_anterior=1.5
+type_tipo_cambio_dia_anterior=type(tipo_cambio_dia_anterior)
+tipo_cambio_dia_anterior_es_float=isinstance(tipo_cambio_dia_anterior,float)
+tipo_cambio_dia_anterior_es_str=isinstance(tipo_cambio_dia_anterior,str)
+print("Variable de tipo cambio dia anterior es :" ,type_tipo_cambio_dia_anterior)
+print("Variable es float:", tipo_cambio_dia_anterior_es_float)
+print("variable es str:", tipo_cambio_dia_anterior_es_str)
+
+
+
+#Ejercicio33
+monto_principal=189.06
+type_monto_principal=type(monto_principal)
+monto_principal_es_float=isinstance(monto_principal,float)
+monto_principal_es_int=isinstance(monto_principal,int)
+print("variable de monto principal es:",type_monto_principal)
+print("variable es float:",monto_principal_es_float)
+print("variable es int:", monto_principal_es_int)
+
+
+#Ejercicio34
+mantenimiento_valor_cuenta_de_ahorro=15.99
+type_mantenimiento_valor_cuenta_de_ahorro=type(mantenimiento_valor_cuenta_de_ahorro)
+mantenimiento_valor_cuenta_de_ahorroo_es_float=isinstance(mantenimiento_valor_cuenta_de_ahorro,float)
+mantenimiento_valor_cuenta_de_ahorro_es_str=isinstance(mantenimiento_valor_cuenta_de_ahorro,str)
+print("Variable de mantenimiento valor cuenta de ahorro es :" ,type_mantenimiento_valor_cuenta_de_ahorro)
+print("Variable es float:", mantenimiento_valor_cuenta_de_ahorro_es_float)
+print("variable es str:", mantenimiento_valor_cuenta_de_ahorro_es_str)
+
+
+#Ejercicio35
+costo_produccion=543
+type_costo_produccion=type(costo_produccion)
+costo_produccion_es_int=isinstance(costo_produccion,int)
+costo_produccionl_es_float=isinstance(costo_produccion,float)
+print("Variable de costo produccion es:",type_costo_produccion)
+print("Variable es int:",costo_produccion_es_int)
+print("Variable es float:",costo_produccion_es_float)
+
+
+
+#Ejercicio36
+costo_indirecto=654.87
+type_costo_indirecto=type(costo_indirecto)
+costo_indirecto_es_float=isinstance(costo_indirecto,float)
+costo_indirecto_es_str=isinstance(costo_indirecto,str)
+print("Variable de costo indirecto es :" ,type_costo_indirecto)
+print("Variable es float:", costo_indirecto_es_float)
+print("variable es str:", costo_indirecto_es_str)
+
+
+#Ejercicio37
+aumento=15
+type_aumento=type(aumento)
+aumento_es_int=isinstance(aumento,int)
+aumento_es_float=isinstance(aumento,float)
+print("variable de aumento es:",type_aumento)
+print("variable es int:",aumento_es_int)
+print("variable es float:",aumento_es_float)
+
+
+#Ejercicio38
+materia_prima_comprada=1000
+type_materia_prima_comprada=type(materia_prima_comprada)
+materia_prima_comprada_es_int=isinstance(materia_prima_comprada,int)
+costo_produccionl_es_float=isinstance(materia_prima_comprada,float)
+print("Variable de materia prima comprada es:",type_materia_prima_comprada)
+print("Variable es int:",materia_prima_comprada_es_int)
+print("Variable es float:",materia_prima_comprada_es_float)
+
+
+#Ejercicio39
+produc_inicial=654.87
+type_produc_inicial=type(produc_inicial)
+produc_inicial_es_float=isinstance(produc_inicial,float)
+produc_inicial_es_str=isinstance(produc_inicial,str)
+print("Variable de produc inicial es :" ,type_produc_inicial)
+print("Variable es float:", produc_inicial_es_float)
+print("variable es str:", produc_inicial_es_str)
+
+
+#Ejercicio40
+materia_prima_utilizada=699
+type_materia_prima_utilizada=type(materia_prima_utilizada)
+materia_prima_utilizada_es_int=isinstance(materia_prima_utilizada,int)
+materia_prima_utilizada_es_float=isinstance(materia_prima_utilizada,float)
+print("variable de materia prima utilizada es:",type_materia_prima_utilizada)
+print("variable es int:",materia_prima_utilizada_es_int)
+print("variable es float:",materia_prima_utilizada_es_float)
+
+#Ejercicio41
+utilidad=654.87
+type_utilidad=type(utilidad)
+utilidad_es_float=isinstance(utilidad,float)
+utilidad_es_str=isinstance(utilidad,str)
+print("Variable de utilidad es :" ,type_utilidad)
+print("Variable es float:", utilidad_es_float)
+print("variable es str:", utilidad_es_str)
+
+
+#Ejercicio42
+importaciones=900
+type_importaciones=type(importaciones)
+importaciones_es_int=isinstance(importaciones,int)
+importaciones_es_float=isinstance(importaciones,float)
+print("variable de importaciones es:",type_importaciones)
+print("variable es int:",importaciones_es_int)
+print("variable es float:",importaciones_es_float)
+
+
+#Ejercicio43
+consumo=699
+type_consumo=type(consumo)
+consumo_es_int=isinstance(consumo,int)
+consumo_es_float=isinstance(consumo,float)
+print("variable de consumo es:",type_consumo)
+print("variable es int:",consumo_es_int)
+print("variable es float:",consumo_es_float)
+
+
+#Ejercicio44
+ted=15.99
+type_ted=type(ted)
+ted_es_float=isinstance(ted,float)
+ted_es_str=isinstance(ted,str)
+print("Variable de ted es :" ,type_ted)
+print("Variable es float:", ted_es_float)
+print("variable es str:", ted_es_str)
+
+
+#Ejercicio45
+plazo_num_cuotas=543
+type_plazo_num_cuotas=type(plazo_num_cuotas)
+plazo_num_cuotas_es_int=isinstance(plazo_num_cuotas,int)
+plazo_num_cuotas_es_float=isinstance(plazo_num_cuotas,float)
+print("Variable de plazo num cuotas es:",type_plazo_num_cuotas)
+print("Variable es int:",plazo_num_cuotas_es_int)
+print("Variable es float:",plazo_num_cuotas_es_float)
+
+
+
+#Ejercicio46
+suma_factores=6786.09
+type_suma_factores=type(suma_factores)
+suma_factores_es_float=isinstance(suma_factores,float)
+suma_factores_es_str=isinstance(suma_factores,str)
+print("Variable de suma factores es :" ,type_suma_factores)
+print("Variable es float:", suma_factores_es_float)
+print("variable es str:", suma_factores_es_str)
+
+
+
+#Ejercicio47
+cuotak=9800.98
+type_cuotak=type(cuotak)
+cuotak_es_int=isinstance(cuotak,int)
+cuotak_es_float=isinstance(cuotak,float)
+print("variable de cuotak es:",type_cuotak)
+print("variable es int:",cuotak_es_int)
+print("variable es float:",cuotak_es_float)
+
+
+#Ejercicio48
+tda_ajustada_plazo=1578.98
+type_tda_ajustada_plazo=type(tda_ajustada_plazo)
+tda_ajustada_plazo_es_float=isinstance(tda_ajustada_plazo,float)
+tda_ajustada_plazo_es_str=isinstance(tda_ajustada_plazo,str)
+print("Variable de tda ajustada plazo es :" ,type_tda_ajustada_plazo)
+print("Variable es float:", tda_ajustada_plazo_es_float)
+print("variable es str:", tda_ajustada_plazo_es_str)
+
+
+#Ejercicio49
+tna=234
+type_tna=type(tna)
+tna_es_int=isinstance(tna,int)
+tna_es_float=isinstance(tna,float)
+print("Variable de tna es:",type_tna)
+print("Variable es int:",tna_es_int)
+print("Variable es float:",tna_es_float)
+
+
+#Ejercicio50
+tasa_efectiva_anual=768.98
+type_tasa_efectiva_anual=type(tasa_efectiva_anual)
+tasa_efectiva_anual_es_float=isinstance(tasa_efectiva_anual,float)
+tasa_efectiva_anual_es_str=isinstance(tasa_efectiva_anual,str)
+print("Variable de tasaefectiva anual es :" ,type_tasa_efectiva_anual
+print("Variable es float:", tasa_efectiva_anual_es_float)
+print("variable es str:", tasa_efectiva_anual_es_str)
